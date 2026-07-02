fe92aae977b17962939ae21ec624350ef7ad99c7
Harsh Raj <raj.harsh4618@gmail.com>
2026-07-02 11:04:28 +0530

    Day 23 Class Assignment

---
+++ Day-23(02-July)/class-demo/src/app/app.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+class Student {
+  id: number;
+  name: string;
+  course: string;
+
+  constructor(id: number, name: string, course: string) {
+    this.id = id;
+    this.name = name;
+    this.course = course;
+  }
+
+  getDetails(): string {
+    return `Student ID: ${this.id} | Name: ${this.name} | Course: ${this.course}`;
+  }
+}
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './app.html',
+  styleUrl: './app.css',
+})
+export class App {
+  student = new Student(1, 'Harsh Rajput', 'Angular');
+
+  details = `Student ID: ${this.student.id} | Name: ${this.student.name} | Course: ${this.student.course}`;
+}
